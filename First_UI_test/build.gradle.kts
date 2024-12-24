f84f89903ae12b3f81e4b30b187e68d249e2c52c
alskhu <alskhu@mail.ru>
2024-12-24 19:14:41 +0300

    add Dependencies

---
+++ First_UI_test/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("io.rest-assured:rest-assured:5.5.0")
+    implementation ("com.google.code.gson:gson:2.11.0")
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor ("org.projectlombok:lombok:1.18.30")
+    testCompileOnly ("org.projectlombok:lombok:1.18.30")
+    testAnnotationProcessor ("org.projectlombok:lombok:1.18.30")
+    implementation("com.codeborne:selenide:7.6.1")
+
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
